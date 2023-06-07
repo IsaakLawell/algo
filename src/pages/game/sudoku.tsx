e8c6c6f09c2f525e423612c2bc31a9b04da90e5c
IsaakLawell <arc.demon1000@gmail.com>
2023-06-07 16:17:10 +0200

    test

--- src/pages/game/sudoku.tsx
+++ src/pages/game/sudoku.tsx
@@ -75,7 +75,7 @@ function Sudoku() {
           </tbody>
         </table>
         <Link href="/" className="buttonLink2">
-          Retour à la page d`&apos`accueil
+          Retour à la page d'accueil
         </Link>
       </div>
     </div>
